a89731fcd735ae088d35d9681153d8ecc9fa89ef
Barroso <goncalogbarroso@gmail.com>
2023-05-02 09:42:53 +0100

    arroz

--- main.c
+++ main.c
@@ -7,8 +7,6 @@
 #include <unistd.h>
 #include "objects.h"
 #include "utils.h"
-
-// .c
 #include "movimento.c"
 #include "objects.c"
 #include "collide.c"
@@ -91,9 +89,6 @@ int main(int argv, char **argc)
     init_pair(2, COLOR_RED, COLOR_RED);
     wattrset(win, COLOR_PAIR(0));
     wattrset(win_game, COLOR_PAIR(1));
-    // init_pair(2, 3, 7);
-    // init_pair(3, 4, 2);
-    // init_pair(4, 7, 4);
     Rect window = {};
     window.tl.x = 0;
     window.tl.y = 0;
@@ -131,35 +126,13 @@ int main(int argv, char **argc)
         werase(win);
         werase(win_game);
         wattrset(win_game, COLOR_PAIR(0));
-        // mvwprintw(win_game, 0, 1, "W:%d H:%d", window_size.x, window_size.y);
         wattrset(win_game, COLOR_PAIR(1));
-        // add_term_line("W:%d H:%d", window_size.x, window_size.y);
-
-
-        // for (int i = 0; i < rects_count; i++)
-        // {
-        //     print_rectangle(win_game, rects[i]);
-        //     Vec2i center = get_center(rects[i]);
-        //     // wattrset(win_game, COLOR_PAIR(0));
-        //     // mvwprintw(win_game, center.y, center.x, "%d", rects[i].color);
-        // }
-
 
         int key = getch();
         add_term_line("%d\n", key);
 
         wattrset(win_game, COLOR_PAIR(1));
         print_bitmap(win_game, window_size, pixmap);
-
-        // mvwprintw(win, 0, 1, "Console");
-        // mvwprintw(win, 1, 1, "Hello");
-        // add_term_line("x:%d y:%d", x, y);
-        // mvwprintw(win_game, circle.center.y, circle.center.x, "o");
-
-        // add_term_line("x:%d y:%d", x, y);
-        // mvwprintw(win_game, circle.center.x, circle.center.y, "o");
-
-        // player = vec2i_add(player, vel);
 
         Rect prev_player = player;
         update(&player, key);
@@ -169,98 +142,12 @@ int main(int argv, char **argc)
         }
 
         print_rectangle(win_game, player);
-
-        // mvwprintw(win_game, player.y, player.x, "?.");
-        // print_circle(win_game, window_size, circle);
-        //  char c = getch();
-        //  if (c == 'w' || c == KEY_UP)
-        //      y -= y_vel;
-        //  if (c == 's' || c == KEY_DOWN)
-        //      y += y_vel;
-        //  if (c == 'a' || c == KEY_LEFT)
-        //      x -= x_vel;
-        //  if (c == 'd' || c == KEY_RIGHT)
-        //      x += x_vel;
-
-        // Line window_bottom = {
-        //     {0, window_size.y - 1},
-        //     {window_size.x - 1, window_size.y - 1}
-        // };
-        // Line window_left = {
-        //     {0, 0},
-        //     {0, window_size.y - 1}
-        // };
-        // Line window_right = {
-        //     {window_size.x - 1, 0},
-        //     {window_size.x - 1, window_size.y - 1}
-        // };
-        // Line window_top = {
-        //     {0, 0},
-        //     {window_size.x, 0}
-        // };
-
-        // Vec2i prev_center = player;
-        //  circle.center = vec2i_add(circle.center, vel);
-
-        // if (collide_circle_line(circle, window_bottom)) {
-        //     Vec2i normal = {0, 1};
-        //     circle.center = prev_center;
-        //     vel = vec2i_add(vel, vec2i_mul_const(vec2i_mul_const(normal, vec2i_dot(vel, normal)), -2));
-        // Line window_bottom = {
-        //     {0, window_size.y},
-        //     {window_size.x, window_size.y}};
-        // Line window_left = {
-        //     {0, 0},
-        //     {0, window_size.y}};
-        // Line window_right = {
-        //     {window_size.x, 0},
-        //     {window_size.x, window_size.y}};
-        // Line window_top = {
-        //     {0, 0},
-        //     {window_size.x, 0}};
-
-        // if (collide_circle_line(circle, window_bottom)) {
-        //     Vec2i normal = {0, 1};
-        //     vel = vec2i_sub(vel, vec2i_mul_const(normal, -2 * vec2i_dot(vel, normal)));
-        // }
-        // else if (collide_circle_line(circle, window_top))
-        // {
-        //     Vec2i normal = {0, -1};
-        //     circle.center = prev_center;
-        //     vel = vec2i_add(vel, vec2i_mul_const(vec2i_mul_const(normal, vec2i_dot(vel, normal)), -2));
-        // }
-        // else if (collide_circle_line(circle, window_left))
-        // {
-        //     Vec2i normal = {-1, 0};
-        //     circle.center = prev_center;
-        //     vel = vec2i_add(vel, vec2i_mul_const(vec2i_mul_const(normal, vec2i_dot(vel, normal)), -2));
-        // }
-        // else if (collide_circle_line(circle, window_right))
-        // {
-        //     Vec2i normal = {1, 0};
-        //     circle.center = prev_center;
-        //     vel = vec2i_add(vel, vec2i_mul_const(vec2i_mul_const(normal, vec2i_dot(vel, normal)), -2));
-        // }
-        // circle.center = vec2i_add(circle.center, vel);
-
-        // refresh();
         render_term(win);
         box(win, 0, 0);
         wrefresh(win);
         wrefresh(win_game);
-        // limit_fps();
-        // usleep(100000);
     }
 
-    // attrset(COLOR_PAIR(2));
-    // print_triangle(9, 15, 4);
-
-    // attrset(COLOR_PAIR(3));
-    // print_triangle(14, 15, 5);
-
-    // attrset(COLOR_PAIR(4));
-    // print_rectangle(20, 12, 1, 6);
-
-    endwin();
+        endwin();
     return 0;
 }
